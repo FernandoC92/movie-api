8476612b25e420f05026dfa9252eea6cf60d2364
Fernando Cordeiro <fernandoagnus1@gmail.com>
2020-11-05 00:11:27 -0300

    docker

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace movieApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
